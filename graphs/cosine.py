6ae9fcea2ad1851a7f63b5ba5242f80d4b63ca9a
nav-github01001 <krishnanav309@gmail.com>
2022-12-02 12:42:56 +0800

    Add cosine

---
+++ graphs/cosine.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plot
+
+# Get x values of the cosine wave
+time = np.arange(0, 10, 0.1);
+
+# Amplitude of the cosine wave is cosine of a variable like time
+amplitude   = np.cos(time)
+
+# Plot a cosine wave using time and amplitude obtained for the cosine wave
+plot.plot(time, amplitude)
+
+# Give a title for the cosine wave plot
+plot.title('cosine wave')
+
+# Give x axis label for the cosine wave plot
+plot.xlabel('Time')
+
+# Give y axis label for the cosine wave plot
+plot.ylabel('Amplitude = sin(time)')
+plot.grid(True, which='both')
+plot.axhline(y=0, color='k')
+
+# Display the cosine wave
+plot.show()
