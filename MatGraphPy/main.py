7b74e6fbf2799e095b3fb5faa347982fc645b84d
smokeynoom <smokeynoom@gmail.com>
2022-12-02 11:50:04 +0800

    changes

--- MatGraphPy/main.py
+++ MatGraphPy/main.py
@@ -1,2 +1,8 @@
-import matplotlib
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+
 print("Welcome from the MatGraphPy Community!")
+
+fig, ax = plt.subplots()  # Create a figure containing a single axes.
+ax.plot([1, 2, 3, 4], [1, 4, 2, 3]);  # Plot some data on the axes. 
